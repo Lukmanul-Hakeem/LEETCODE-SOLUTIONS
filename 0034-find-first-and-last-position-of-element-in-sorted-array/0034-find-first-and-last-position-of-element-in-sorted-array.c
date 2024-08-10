079a85e55c45bea0fadbb26294fe040c49d89e30
LUKMANUL HAKEEM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2024-08-11 01:16:03 +0530

    Time: 3 ms (95.94%), Space: 7.5 MB (43.4%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
@@ -0,0 +1,30 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int binarySearch(int A[],int low,int high,int target,int ans){
+    if(low>high){
+        return ans;
+    }
+    int mid=(low+high)/2;
+    if(A[mid]<=target){
+        return binarySearch(A,mid+1,high,target,ans);
+    }else{
+        ans=mid;
+        return binarySearch(A,low,mid-1,target,ans);
+    }
+}
+int* searchRange(int* nums, int size, int target, int* returnSize) {
+    int *ans=(int*)malloc(2*sizeof(int));
+    int start=binarySearch(nums,0,size-1,target-1,size);
+    int end=binarySearch(nums,0,size-1,target,size);
+    *returnSize=2;
+    if(start==end){
+        ans[0]=-1;
+        ans[1]=-1;
+       // return [-1,-1];
+    }else{
+        ans[0]=start;
+        ans[1]=end-1;
+    }
+    return ans;
+}
