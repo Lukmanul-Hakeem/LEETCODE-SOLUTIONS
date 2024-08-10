e23e92146ab59c3d6a87e47f0ae11bcbde3a3a7a
LUKMANUL HAKEEM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2024-08-11 00:57:59 +0530

    Time: 129 ms (10.81%), Space: 8.4 MB (53.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,30 +1,3 @@
-// /**
-//  * Note: The returned array must be malloced, assume caller calls free().
-//  */
-// int* twoSum(int A[], int size, int target, int* returnSize) {
-//     int *index=(int *)malloc(2*sizeof(int));
-//     int i=0,j=size-1;
-//     *returnSize=2;
-//     while(i<j){
-//         if(A[i]+A[j]==target){
-//             index[0]=i;
-//             index[1]=j;
-//             return index;
-//         }else{
-//             if(A[i]+A[j]>target){
-//                 j--;
-//             }else{
-//                 i++;
-//             }
-//         }
-//     }
-//     free(index);
-//     return NULL;
-// }
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 int* twoSum(int* num, int size, int target, int* returnSize) {
     int* result = NULL;
     result = (int*)malloc(2 * sizeof(int));
