9c83459eba58b19d6372000c2c46dae7a0860998
LUKMANUL HAKEEM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2024-09-08 23:28:07 +0530

    Time: 0 ms (100%), Space: 42.5 MB (99.3%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,24 +14,16 @@
  * }
  */
 class Solution {
-    public void traverse(TreeNode root,TreeNode prev,List<Integer> list, List<List<Integer>> L,int sum,int target){
-        if(root==null){
-            //if(prev.left==null && prev.right==null && sum==target)if(!L.contains(list))L.add(new ArrayList(list));
-            return;
-        }
-        sum+=root.val;
-        list.add(root.val);
-        if(root.left==null && root.right==null && sum==target)L.add(new ArrayList(list));
-        traverse(root.left,root,list,L,sum,target);
-        traverse(root.right,root,list,L,sum,target);
-        list.remove(list.size()-1);
-    }
-    public boolean hasPathSum(TreeNode root, int target) {
-        List<Integer> list=new ArrayList<>();
-        List<List<Integer>> listoflist=new ArrayList<>();
+    public boolean traversal(TreeNode root,int sum,int target){
         if(root==null)return false;
-        traverse(root,null,list,listoflist,0,target);
-        if(listoflist.size()!=0)return true;
-        return false;
+        sum+=root.val;
+        if(root.left==null && root.right==null){
+            if(sum==target)return true;
+        }
+        return traversal(root.left,sum,target) || traversal(root.right,sum,target);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        //int sum[]=new int [1];
+        return traversal(root,0,targetSum);
     }
 }
