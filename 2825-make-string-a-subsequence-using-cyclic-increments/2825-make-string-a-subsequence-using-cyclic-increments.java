d5641b1eb295aabb5fc5c8026600d9b642eae5ed
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2024-12-04 10:01:53 +0530

    Time: 6 ms (71.31%), Space: 45.8 MB (23.03%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
         if(str1.length()<str2.length())return false;
+        int l1=str1.length();
+        int l2=str2.length();
         int i=0;
         int j=0;
-        while(i<str1.length() && j<str2.length()){
+        while(i<l1 && j<l2){
 
             char ch1=str1.charAt(i);
             char ch2=(char)((ch1=='z') ? 'a' : (ch1+1));
@@ -12,7 +14,7 @@ class Solution {
             i++;
         }
         
-        return j==str2.length();
+        return j==l2;
 
     }
 }
