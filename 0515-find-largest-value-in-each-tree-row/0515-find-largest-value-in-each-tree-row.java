bb7b2eb736a8e485fcb1844c6ea553bdaf4dd639
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2024-12-25 11:11:03 +0530

    Time: 1 ms (94.48%), Space: 45.4 MB (64.94%) - LeetHub

---
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void findMaxOfEachRow(TreeNode root,List<Integer> list,int level){
+        if(root == null)return;
+        
+        if(list.size()==0){
+            list.add(root.val);
+            // System.out.println("hello");
+        }
+        else if(list.size()-1>=level && list.get(level)<root.val){
+            list.set(level,root.val);
+            // System.out.println("hello...");
+        }else if(list.size()==level){
+            list.add(root.val);
+        }
+        // if(list.size()-1>=level)System.out.println(list.size()-1+" "+list.get(level)+" "+root.val+" "+level);
+        // System.out.println(list.size()-1+" "+level+" "+root.val);
+        findMaxOfEachRow(root.left,list,level+1);
+        findMaxOfEachRow(root.right,list,level+1);
+    }
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> list =new ArrayList<>();
+        findMaxOfEachRow(root,list,0);
+        return list;
+    }
+}
