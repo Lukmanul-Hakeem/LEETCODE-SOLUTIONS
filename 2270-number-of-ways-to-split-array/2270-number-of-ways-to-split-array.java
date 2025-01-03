9eae7d28c2450b489c022181cf9c08f729df3e4a
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2025-01-03 10:01:08 +0530

    Time: 5 ms (5.15%), Space: 58.1 MB (79.25%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,11 +1,21 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long prefixSum[]=new long[nums.length];
-        prefixSum[0]=nums[0];
-        for(int i=1;i<nums.length;i++)prefixSum[i]=nums[i]+prefixSum[i-1];
-        // for(int i=0;i<nums.length;i++)System.out.println(prefixSum[i]);
+        // long prefixSum[]=new long[nums.length];
+        // prefixSum[0]=nums[0];
+        // for(int i=1;i<nums.length;i++)prefixSum[i]=nums[i]+prefixSum[i-1];
+        // int count=0;
+        // for(int i=0;i<nums.length-1;i++) if(prefixSum[i] >= prefixSum[prefixSum.length-1]-prefixSum[i])count++; 
+        // return count;
+
+        long sum=0;
+        long curSum=0;
         int count=0;
-        for(int i=0;i<nums.length-1;i++) if(prefixSum[i] >= prefixSum[prefixSum.length-1]-prefixSum[i])count++; 
+        for(int i=0;i<nums.length;i++)sum+=nums[i];
+        System.out.println(sum);
+        for(int i=0;i<nums.length-1;i++){
+            curSum+=nums[i];
+            if(curSum >= sum -curSum)count++;
+        }
         return count;
 
     }
