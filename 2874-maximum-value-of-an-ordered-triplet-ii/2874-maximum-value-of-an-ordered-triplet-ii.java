e3d94dde7d745871b3ede8a9374588a1c06a51a5
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2025-04-03 12:00:51 +0530

    Time: 1130 ms (5.91%), Space: 56.6 MB (98.39%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.java
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.java
@@ -5,28 +5,22 @@ class Solution {
         prefix[0]=nums[0];
         suffix[nums.length-1]=nums[nums.length-1];
 
-        // for(int i =1;i<nums.length;i++){
-        //     prefix[i] = prefix[i-1] > nums[i] ? prefix[i-1] : nums[i];
-        //     suffix[nums.length-1-i] = suffix[nums.length-i] > nums[nums.length-1-i] ? suffix[nums.length-i] : nums[nums.length-1-i];
-        // }
-        int max1=nums[0];
-        int max2=nums[nums.length-1];
-        for(int i =1;i<nums.length;i++){
-            
-            prefix[i] = max1;
-            max1 = prefix[i-1] > nums[i] ? prefix[i-1] : nums[i];
+        int n = nums.length;
+        int max1=0,max2=0;
+        prefix[0]=suffix[n-1]=0;
+        for(int i=1;i<n;i++){
+            max1 = max1 > nums[i-1] ? max1 : nums[i-1];
+            prefix[i]=max1;
 
-            suffix[nums.length-1-i] = max2;
-            max2 = suffix[nums.length-i] > nums[nums.length-1-i] ? suffix[nums.length-i] : nums[nums.length-1-i];
-
-            // suffix[nums.length-1-i] = suffix[nums.length-i] > nums[nums.length-1-i] ? suffix[nums.length-i] : nums[nums.length-1-i];
+            max2 = max2 > nums[n-i] ? max2 : nums[n-i];
+            suffix[n-1-i] = max2;
         }
-
         for(int val : suffix)System.out.println(val);
-        long max=0;
-        for(int i =0;i<nums.length;i++){
-            System.out.println(prefix[i]+" "+nums[i]+" "+suffix[i]+" = "+(long)(prefix[i]-nums[i])*suffix[i]);
-            if(prefix[i]!=nums[i] && suffix[i]!=nums[i])max = max > (long)(prefix[i]-nums[i])*suffix[i] ? max : (long)(prefix[i]-nums[i])*suffix[i];
+
+        long max = 0;
+
+        for(int i=1;i<n;i++){
+            max = max > (long)(prefix[i]-nums[i])*suffix[i] ? max : (long)(prefix[i]-nums[i])*suffix[i];
         }
 
         return max;
