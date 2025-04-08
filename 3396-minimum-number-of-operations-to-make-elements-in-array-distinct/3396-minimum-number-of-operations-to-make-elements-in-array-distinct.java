5bec0f56ca828cd51dc04e043410dd7f4830c91c
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2025-04-08 18:35:28 +0530

    Time: 1 ms (98.39%), Space: 44.5 MB (50.97%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,18 +1,10 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-
         int map[] = new int[101];
-        for(int i=0;i<nums.length;i++)map[nums[i]]=0;
-        int index =-1;
-        for(int i = nums.length-1;i>=0;i--){
-            if(map[nums[i]]==1){
-                index = i;
-                break;
-            }else map[nums[i]]++;
-        }
-
-        index++;
-        return index == -1 ? 0 : (index%3 == 0 ? index/3 : index/3+1);
-
+        // for(int i=0;i<nums.length;i++)map[nums[i]]=0;
+        for(int i = nums.length-1;i>=0;i--)
+            if(map[nums[i]]==1)return (i+1)%3 == 0 ? (i+1)/3 : (i+1)/3+1;
+            else map[nums[i]]++; 
+        return 0;
     }
 }
