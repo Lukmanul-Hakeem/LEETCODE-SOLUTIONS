b3d98f2fa7b029a1d01cc8a0560d1c50ab826098
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2025-05-16 16:13:03 +0530

    Time: 121 ms (7.99%), Space: 45.9 MB (8.3%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,34 @@
 class Solution {
-    public int findMinCount(int nums[],int dp[],int index){
-        if(index == nums.length-1)return 0;
-        if(dp[index]!=-1)return dp[index]+1;
-        int min = Integer.MAX_VALUE-10000;
-        int val = 0;
-        for(int i=index+1;i<nums.length && i<=index+nums[index];i++){
-            val = findMinCount(nums,dp,i);
-            //System.out.println(nums[index]+" "+index+" "+val+" "+i);
-            min = min < val ? min : val;
+    public int findMin(int nums[],int dp[],int index,int maxInd){
+
+        if(index >= nums.length -1)return 1;
+        if(dp[index] != -1)return dp[index] + 1;
+
+
+        maxInd = maxInd > index + nums[index] ? maxInd : index + nums[index];
+
+        // System.out.println(maxInd);
+        if(maxInd <= index)return 100_01;
+
+        if(maxInd >= nums.length-1){
+            dp[index] = 1;
+            return dp[index] + 1;
         }
-        //System.out.println()
-        dp[index]=min;
-        return dp[index]+1;
+
+        int minVal = 100_01;
+        int val = 0;
+        for(int i=index + 1; i <= index + nums[index] && i < nums.length; i++){
+            val = findMin(nums,dp,i,maxInd);
+            minVal = val < minVal ? val : minVal;
+        }
+
+        return (dp[index] = minVal) + 1;
+
     }
     public int jump(int[] nums) {
-        int dp[] = new int [nums.length];
-        for(int i = 0;i<nums.length;i++)dp[i]=-1;
-        dp[0]=findMinCount(nums,dp,0);
-        for(int i = 0;i<nums.length;i++)System.out.println(dp[i]);
-        return dp[0];
+        int dp[] = new int[nums.length+1];
+        for(int i=0;i<nums.length;i++)dp[i] = -1;
+        findMin(nums,dp,0,0);
+        return dp[0] == -1 ? 0 : dp[0];
     }
 }
