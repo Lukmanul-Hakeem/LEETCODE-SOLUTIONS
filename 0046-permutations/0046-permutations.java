3127ff3133a23679f4178597dba9895d913c62b3
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2025-06-03 16:09:20 +0530

    Time: 9 ms (2.59%), Space: 44.8 MB (30.94%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void find(int nums[],int index,List<List<Integer>> result,List<Integer> list){
+    public void find(int nums[],List<List<Integer>> result,List<Integer> list){
         if(list.size() == nums.length){
             if(!result.contains(list)){
                 result.add(new ArrayList(list));
@@ -10,7 +10,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(!list.contains(nums[i])){
                 list.add(nums[i]);
-                find(nums,i+1,result,list);
+                find(nums,result,list);
                 list.remove(list.size()-1);
             }
         }
@@ -19,7 +19,7 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        find(nums,0,result,list);
+        find(nums,result,list);
         return result;
     }
 }
