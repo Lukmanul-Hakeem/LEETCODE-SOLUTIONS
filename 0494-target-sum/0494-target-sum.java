362dd5ce3639ef81e0bd12f919a523fbfccce820
LUKMANKM <145091843+LUKMANULHAKEEM-prg@users.noreply.github.com>
2025-06-10 12:18:15 +0530

    Time: 6 ms (77.56%), Space: 43.2 MB (52.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,26 @@
 class Solution {
-    public void findCount(int nums[],int target,int sum,int count[],int index){
-        // System.out.println(sum);
-        if(index==nums.length){
-            if(sum==target)count[0]++;
-            return;
+    public int count(int nums[],int index,int target,int dp[][]){
+        // if(target == 0)return 1;
+        if(index == nums.length){
+            return target == 0 ? 1 : 0;
         }
-        findCount(nums,target,sum+nums[index],count,index+1);
-        findCount(nums,target,sum-nums[index],count,index+1);
+        if(dp[index][target] != -1)return dp[index][target];
+        int notPick = count(nums,index+1,target,dp);
+        int pick = 0;
+        if(nums[index] <= target)pick = count(nums,index+1,target - nums[index],dp);
+
+        return dp[index][target] = notPick + pick;
+        
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int count[]=new int [1];
-        count[0]=0;
-        findCount(nums,target,0,count,0);
-        return count[0];
+        int sum = 0;
+        for(int val : nums)sum+=val;
+        if((sum - target)%2 == 1 || (sum - target)<0 )return 0;
+        System.out.println((sum - target)%2);
+
+        int dp[][] = new int[nums.length][(sum - target)/2 + 1];
+        for(int i=0;i<nums.length;i++)Arrays.fill(dp[i],-1);
+        System.out.println((sum - target)/2);
+        return count(nums,0,(sum - target)/2,dp);
     }
 }
