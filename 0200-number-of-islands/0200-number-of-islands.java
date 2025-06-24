f1bb7d7bfa63e51610ac1014cc0f6b6d8ead506e
LUKMANKM <145091843+Lukmanul-Hakeem@users.noreply.github.com>
2025-06-24 23:33:33 +0530

    Time: 3 ms (84.61%), Space: 49.5 MB (61.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,24 @@
 class Solution {
-    public void findCount(char [][]grid,int row,int col){
-        if(col==grid[0].length || row==grid.length || col==-1 || row==-1 || grid[row][col]=='0'){
-            return;
-        }
-        grid[row][col]='0';
-        findCount(grid,row,col+1);
-        findCount(grid,row+1,col);
-        findCount(grid,row,col-1);
-        findCount(grid,row-1,col);
+    public void Traverse(char grid[][],int row,int col){
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0')return;
+        grid[row][col] = '0';
+        Traverse(grid,row+1,col);
+        Traverse(grid,row,col+1);
+        Traverse(grid,row,col-1);
+        Traverse(grid,row-1,col);
         
     }
     public int numIslands(char[][] grid) {
-        int count=0;
+        int count = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1'){
-                    findCount(grid,i,j);
+                if(grid[i][j] == '1'){
                     count++;
+                    Traverse(grid,i,j);
                 }
             }
         }
         return count;
+
     }
 }
